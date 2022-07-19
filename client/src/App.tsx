b8db6ca571fde13dfb8a0c47bd570cea60b7232b
vishwaksenan <vishwaksenang@gmail.com>
2022-07-19 19:35:14 +0100

    style-skeleton

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,22 +3,50 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const notesTitle = ['Title 1', 'Title 2', 'Title 3']
+  const notes = [{
+      title:'Title 1',
+      description: 'Description 1'
+    },
+    {
+      title: 'Title 2',
+      description: 'Description 2'
+    },
+    {
+      title: 'Title 2',
+      description: 'Description 2'
+    }
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="notes-title">
+        <div className='note-card-container'>
+          <div className="note-card">
+            <p>Title 1</p>
+          </div>
+          <div className="note-card">
+            <p>Title 2</p>
+          </div>
+          <div className="note-card">
+            <p>Title 3</p>
+          </div>
+        </div>
+        <div>
+          <button>Create a new</button>
+        </div>
+      </div>
+      <div className="notes-view">
+        <div className="note-title-view" contentEditable={true}>
+          <p>Title 1</p>
+        </div>
+        <div className="note-description" contentEditable={true}>
+          <p>Description 1</p>
+        </div>
+        <div className="button-section">
+          <button>Update</button>
+          <button>Delete</button>
+        </div>
+      </div>
     </div>
   );
 }
